333bcfd0fff81f0ed9684cfdda1a93de40c89132
ZhangZeYing <340032382@qq.com>
2020-09-11 15:29:34 +0800

    添加常量类

--- north_online/lib/core/constant/color_constant.dart
+++ north_online/lib/core/constant/color_constant.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class ColorConstant {
   static const Color normalBalckColor = Color(0xff333333);
   static const Color normalGrayColor = Color(0xff999999);
+  static const Color normalEEEColor = Color(0xffeeeeee);
   static const Color themeColor = Color(0xffd77b6e);
 }
---
+++ north_online/lib/core/constant/screen_constant.dart
@@ -0,0 +1,6 @@
+class ScreenConstant {
+  // final BuildContext context;
+  // const ScreenConstant(this.context);
+  // static final double screen_height = MediaQuery.of(context).size.height;
+  // static final double screen_width = MediaQuery.of(context).size.width;
+}
